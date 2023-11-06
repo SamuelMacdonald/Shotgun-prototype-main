48906c7af824a3c98ee989dfe2e9c65c2d20a3fc
SamuelMacdonald <146819235+SamuelMacdonald@users.noreply.github.com>
2023-11-06 16:09:53 +0000

    added enemy and fixed movement

--- Assets/Script/Shotgun prototype.cs
+++ Assets/Script/Shotgun prototype.cs
@@ -16,7 +16,7 @@ public class Shotgunprototype : MonoBehaviour
     void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = mousePosition - transform.position; 
+        Vector2 direction = mousePosition - transform.position;
         float angle = Vector2.SignedAngle(Vector2.right, direction);
         Vector3 targetRotation = new Vector3(0, 0, angle);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(targetRotation), turnSpeed * Time.deltaTime);
--- Assets/Script/player.cs
+++ Assets/Script/player.cs
@@ -6,14 +6,14 @@ public class player : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D rb;
-    public int ve;
+    public float ve;
     public int bullet;
-    public float maxSpeed;
+   public float maxSpeed;
     
     void Start()
     {
-      rb = GetComponent<Rigidbody2D>(); 
-      
+      rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
@@ -22,15 +22,17 @@ public class player : MonoBehaviour
         
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
+
         Debug.Log(rb.velocity.magnitude);
-        if(rb.velocity.magnitude > maxSpeed )
+        if(rb.velocity.magnitude > maxSpeed)
         {
-            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);    
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
         }
+        
         
         if (Input.GetMouseButtonDown(0) && bullet >= 1 )
         {
-            rb.AddForce(-mousePosition * ve);
+            rb.velocity = -mousePosition * ve;
             bullet--;
         }
         if (Input.GetKeyDown("r") && bullet == 0)
